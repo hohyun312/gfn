705d296799ea00c4efdec580e9b4e0211adfd619
Hohyun Kim <hohyun312@gmail.com>
2023-06-21 18:08:11 +0900

    Add option for TBModel:

--- src/model.py
+++ src/model.py
@@ -4,10 +4,15 @@ from torch.distributions import Categorical
 
 
 class TBModel(nn.Module):
-    def __init__(self, ndim, hidden_dims=[20, 20]):
+    def __init__(self, ndim, hidden_dims=[20, 20], uniform_backward_flow=False):
         super().__init__()
         self.ndim = ndim
-        dims = [self.ndim] + hidden_dims + [2 * self.ndim]
+        self.uniform_backward_flow = uniform_backward_flow
+        if uniform_backward_flow:
+            output_dim = self.ndim
+        else:
+            output_dim = 2 * self.ndim
+        dims = [self.ndim] + hidden_dims + [output_dim]
         modules = []
         for i in range(len(dims) - 1):
             modules += [nn.Linear(dims[i], dims[i + 1])]
@@ -19,7 +24,12 @@ class TBModel(nn.Module):
     def forward(self, states):
         x = torch.tensor(states).float().to(self.device)
         logits = self.mlp(x)
-        P_F, P_B = logits.chunk(2, dim=-1)
+        if not self.uniform_backward_flow:
+            P_F, P_B = logits.chunk(2, dim=-1)
+        else:
+            P_F = logits
+            P_B = torch.ones_like(logits)
+            
         P_F = P_F * (1 - x) + x * -100
         P_B = P_B * x + (1 - x) * -100
         return P_F, P_B
